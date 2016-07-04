27abd23e10779c8f31518b46fc0330bab4c1d656
Diego Javier Zea <diegozea@gmail.com>
2016-07-04 19:18:41 -0300

    New Results Format

--- MIToS/Suite/Print_Small_Benchmark.jl
+++ MIToS/Suite/Print_Small_Benchmark.jl
@@ -6,21 +6,22 @@ res = JLD.load("result_SmallBenchmark.jld")
 for (t,d,l) in [("#### MSA module","MIToS.MSA.","small_bench_msa"),
               ("#### PDB module","MIToS.PDB.","small_bench_mitos_pdb"),
               ("#### Information module","MIToS.Information.","small_bench_information")]
-    println("")
-    println(t)
+    println("  ")
+    println(t, "  ")
     for (k,v) in res["small_result"][l]
-        println("")
-        println("|", k, "| |")
-        println("|-|-|")
+        println("  ")
+        println("| ", k, " | |  ")
+        println("|---|---|  ")
         for (kk,vv) in minimum(v)
-            println("|", replace(kk,d,"") , "|", BenchmarkTools.prettytime(time(vv)), "|")
+            println("| ", replace(kk,d,"") , " | ", BenchmarkTools.prettytime(time(vv)), " |  ")
         end
     end
 end
 
-println("#### Pfam/pipeline\n")
-println("|-|-|")
+println("#### Pfam/pipeline  ")
+println("  ")
+println("|---|---|  ")
 for (k,v) in minimum(res["small_result"]["small_bench_pfam"])
-    println("|", replace(k,"MIToS.Pfam.","") , "|", BenchmarkTools.prettytime(time(v)), "|")
+    println("| ", replace(k,"MIToS.Pfam.","") , " | ", BenchmarkTools.prettytime(time(v)), " |  ")
 end
 
